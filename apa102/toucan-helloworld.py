4ed4fef440dcba0be0f3b75c0e0296f9dbcf08aa
200ok <200ok@noreply.bitbucket.org>
2021-06-06 23:22:34 +1000

    Updated instructions to run a simple hello world first

---
+++ apa102/toucan-helloworld.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+from apa102 import APA102
+import RPi.GPIO as GPIO
+import time
+
+# Use try/catch/finally to ensure the GPIO always gets cleaned up.
+try:
+    print("Toucan: hello world! Note LED brightness is turned down to 0.1. This example uses the APA102 API directly, without any helpers.")
+    # Initialise lights with APA210
+    lights = APA102(8, 23, 24, brightness=0.1)
+
+    print("Toucan: setting pixels to ROYGBIV+white.")
+    lights.set_pixel(0, 255, 0, 0)
+    lights.set_pixel(1, 255, 165, 0)
+    lights.set_pixel(2, 255, 255, 0)
+    lights.set_pixel(3, 0, 128, 0)
+    lights.set_pixel(4, 0, 0, 255)
+    lights.set_pixel(5, 75, 0, 130)
+    lights.set_pixel(6, 238, 130, 238)
+    lights.set_pixel(7, 255, 255, 255)
+
+    print("Toucan: calling show() to update the Toucan with the pixels we just set")
+    lights.show()
+
+    print("Toucan: a little pause to admire...")
+    time.sleep(1)
+
+    print("Toucan: now turning them all off again")
+    lights.set_pixel(0, 0, 0, 0)
+    lights.set_pixel(1, 0, 0, 0)
+    lights.set_pixel(2, 0, 0, 0)
+    lights.set_pixel(3, 0, 0, 0)
+    lights.set_pixel(4, 0, 0, 0)
+    lights.set_pixel(5, 0, 0, 0)
+    lights.set_pixel(6, 0, 0, 0)
+    lights.set_pixel(7, 0, 0, 0)
+    lights.show()
+    
+except Exception as e: 
+    print("Toucan: well we certainly hoped we wouldn't up here, but something has gone wrong.")
+    print(e)
+
+finally:
+    print("Toucan: cleaning up GPIO")
+    GPIO.cleanup()
+    print("Toucan: done!")
--- apa102/toucan.py
+++ apa102/toucan.py
@@ -1,34 +1,9 @@
 #!/usr/bin/env python3
-import toucan_helpers as toucan
-import time
+import subprocess
 
-print("toucan.py")
+subprocess.run("./toucan-helloworld.py")
+subprocess.run("./toucan-red.py")
+subprocess.run("./toucan-orange.py")
+subprocess.run("./toucan-green.py")
+subprocess.run("./toucan-fade.py")
 
-try:
-    lights = toucan.setup()
-
-    lights.set_pixel(0, 255, 0, 0)
-    lights.set_pixel(1, 255, 165, 0)
-    lights.set_pixel(2, 255, 255, 0)
-    lights.set_pixel(3, 0, 128, 0)
-    lights.set_pixel(4, 0, 0, 255)
-    lights.set_pixel(5, 75, 0, 130)
-    lights.set_pixel(6, 238, 130, 238)
-    lights.set_pixel(7, 255, 255, 255)
-    lights.show()
-    time.sleep(1)
-
-    toucan.fillUp(lights, 0, 255, 0)
-    toucan.fillDown(lights, 0, 255, 255)
-    toucan.fillRandom(lights)
-    time.sleep(1)
-
-    toucan.fadeUp(lights)
-    toucan.fadeDown(lights)
-
-except Exception as e: 
-    print(e)
-
-finally:
-    toucan.lightsOut(lights)
-    toucan.gpioClean()
